f879123de159dd2521ee65ee72e232b179615463
TylerDrd <116826632+TylerDrd@users.noreply.github.com>
2023-12-09 09:34:38 +0530

    Create Move Zeroes.cpp
    Question Link - 
    https://leetcode.com/problems/move-zeroes/

---
+++ Arrays/Easy/Move Zeroes.cpp
@@ -0,0 +1,27 @@
+//Time Complexity -  O(n)
+//Space Complexity - O(1)
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size(),j=-1;
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]==0) 
+            {
+                j=i;
+                break;
+            }
+        }
+        if(j==-1) return ;
+        for(int i=j+1;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+    }
+};
