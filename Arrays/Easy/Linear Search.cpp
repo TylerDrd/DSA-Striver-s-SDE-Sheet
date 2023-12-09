65f12ca41f2f276a2b70c8cfcb6bd936197fb218
TylerDrd <116826632+TylerDrd@users.noreply.github.com>
2023-12-09 09:36:48 +0530

    Linear Search.cpp
    Question Link - 
    https://www.codingninjas.com/studio/problems/linear-search_6922070?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf

---
+++ Arrays/Easy/Linear Search.cpp
@@ -0,0 +1,17 @@
+//Time Complexity - O(n)
+//Space Complexity - O(1)
+
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    // Write your code here.
+    int j=-1;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==num)
+        {
+            j=i;
+            break;
+        }
+    }
+    return j;
+}
