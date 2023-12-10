456b3bff364e69eb5b9724ac1ea2bcf220ba9c0e
TylerDrd <116826632+TylerDrd@users.noreply.github.com>
2023-12-10 11:18:43 +0530

    Create Insert Node At The Beginning.cpp
    Question link - 
    https://www.codingninjas.com/studio/problems/insert-node-at-the-beginning_8144739?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=SUBMISSION

---
+++ Linked List/1D LL/Insert Node At The Beginning.cpp
@@ -0,0 +1,23 @@
+//Time complexity - O(1)
+//Space Complexity - O(1)
+
+
+/**
+ * Definition of linked list
+ * class Node {
+ *
+ * public:
+ *     int data;
+ *     Node* next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node* next) : data(x), next(next) {}
+ * };
+ */
+
+Node* insertAtFirst(Node* list, int newValue) {
+    // Write your code here
+    Node* head = new Node(newValue);
+    head->next = list;
+    return head;
+}
