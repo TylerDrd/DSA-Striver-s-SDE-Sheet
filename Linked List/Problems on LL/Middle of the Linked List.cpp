23a221af4a7ca53a3803d116615c57715389743a
TylerDrd <116826632+TylerDrd@users.noreply.github.com>
2023-12-10 14:46:47 +0530

    Middle of the Linked List.cpp
    question link - 
    https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ Linked List/Problems on LL/Middle of the Linked List.cpp
@@ -0,0 +1,16 @@
+//time complexity - O(n/2)
+//space complexity - O(1)
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        return slow;
+    }
+};
