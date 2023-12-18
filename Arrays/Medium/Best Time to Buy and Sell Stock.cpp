e8aca7658d222656d4b3268c982d89538163a7bb
TylerDrd <116826632+TylerDrd@users.noreply.github.com>
2023-12-18 22:04:39 +0530

    Create Best Time to Buy and Sell Stock.cpp
    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/

---
+++ Arrays/Medium/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+//time complexity - O(n)
+//space complexity - O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int m = prices[0],p=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            m=min(m,prices[i]);
+            p=max(p,prices[i]-m);
+        }
+        return p;
+    }
+};
