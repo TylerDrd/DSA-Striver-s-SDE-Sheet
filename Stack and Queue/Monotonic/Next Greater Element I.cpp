b3e56646ab40d9f8d85e00947b6c101f3b1b3e47
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2023-12-29 14:17:13 +0530

    Update Next Greater Element I.cpp

--- Stack and Queue/Monotonic/Next Greater Element I.cpp
+++ Stack and Queue/Monotonic/Next Greater Element I.cpp
@@ -4,26 +4,64 @@ public:
         int n2=nums2.size();
         unordered_map<int,int> m;
         stack<int> s;
-        for(int i=0;i<n2;i++)
+
+        s.push(-1);
+
+        for(int i=n2-1;i>=0;i--)
         {
-            if(s.empty() || s.top() > nums2[i]) s.push(nums2[i]);
-            else
+            if( s.top() < nums2[i])
             {
-                while( !s.empty() && s.top()<nums2[i])
+                while(!s.empty() && s.top()<nums2[i])
                 {
-                    m[s.top()] = nums2[i];
                     s.pop();
                 }
-                s.push(nums2[i]);
+                if(s.empty()) s.push(-1);   
+                m[nums2[i]] = s.top();
+                
             }
+            else
+            {
+                m[nums2[i]]=s.top();
+            }
+            s.push(nums2[i]);
         }
-        int n1=nums1.size();
-        vector<int> op(n1,-1);
-        for(int i=0;i<n1;i++)
+        vector<int> t;
+        for(int i=0;i<nums1.size();i++)
         {
-            if(m.find(nums1[i]) != m.end()) op[i]=m[nums1[i]];
-            else op[i]=-1;
+            t.push_back(m[nums1[i]]);
         }
-        return op;
+        return t;
     }
+    
 };
+
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+//         int n2=nums2.size();
+//         unordered_map<int,int> m;
+//         stack<int> s;
+//         for(int i=0;i<n2;i++)
+//         {
+//             if(s.empty() || s.top() > nums2[i]) s.push(nums2[i]);
+//             else
+//             {
+//                 while( !s.empty() && s.top()<nums2[i])
+//                 {
+//                     m[s.top()] = nums2[i];
+//                     s.pop();
+//                 }
+//                 s.push(nums2[i]);
+//             }
+//         }
+//         int n1=nums1.size();
+//         vector<int> op(n1,-1);
+//         for(int i=0;i<n1;i++)
+//         {
+//             if(m.find(nums1[i]) != m.end()) op[i]=m[nums1[i]];
+//             else op[i]=-1;
+//         }
+//         return op;
+//     }
+// };
