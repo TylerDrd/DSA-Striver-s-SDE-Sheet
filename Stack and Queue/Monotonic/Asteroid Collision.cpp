84d196868f9a39d329bc7cda9afbda6a20644f4a
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2023-12-30 14:00:11 +0530

    Create Asteroid Collision.cpp

---
+++ Stack and Queue/Monotonic/Asteroid Collision.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> ans;
+        stack<int> s;
+        for(int i=0;i<asteroids.size();i++){
+            if(s.empty() || asteroids[i]>0)
+            {
+                s.push(asteroids[i]);
+            }
+            else
+            {
+                while(!s.empty() && s.top()>0 && s.top()<abs(asteroids[i])){
+                    s.pop();
+                }
+                if(!s.empty() && s.top()==abs(asteroids[i])){
+                    s.pop();
+                }
+                else
+                {
+                    if(s.empty() || s.top()<0)
+                    s.push(asteroids[i]);
+                }
+            }
+        }
+        while(!s.empty()){
+            ans.push_back(s.top());
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    
+    }
+};
