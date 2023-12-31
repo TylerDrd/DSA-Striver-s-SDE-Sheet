3cbe1d94fc40534ea6e1947ec2ba91e777e04b70
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2023-12-31 10:19:09 +0530

    Create Sliding Window Maximum.cpp

---
+++ Stack and Queue/Implementation Problems/Sliding Window Maximum.cpp
@@ -0,0 +1,22 @@
+//time complexity - O(n)(traversal) + O(n) (removal from front)
+//space complexity - O(k) - at any instant , dequeue has k elements
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<int> q;
+        vector<int> ans;
+
+        for(int i =0; i<n;i++)
+        {
+            if(!q.empty() && q.front()==i-k) q.pop_front();
+
+            while(!q.empty() && nums[q.back()] < nums[i]) q.pop_back();
+            q.push_back(i);
+            
+            if(i>=k-1) ans.push_back(nums[q.front()]);
+        }
+        return ans;
+    }
+};
