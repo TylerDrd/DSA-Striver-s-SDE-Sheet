55665fea51e29d87ed36193d83b056a80a810281
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2024-01-15 14:31:29 +0530

    Create Maximum Depth of Binary Tree.cpp

---
+++ Binary Trees/Medium Problems/Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,12 @@
+//space complexity - O(height) - recursive stack space
+
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL) return 0;
+
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        return 1+max(lh,rh);
+    }
+};
