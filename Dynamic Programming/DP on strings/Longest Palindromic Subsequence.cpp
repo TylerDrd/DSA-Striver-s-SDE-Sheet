89d43dc0025ce56440de3e11b079c6ca071d70ef
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2024-03-05 00:22:23 +0530

    Create Longest Palindromic Subsequence.cpp

---
+++ Dynamic Programming/DP on strings/Longest Palindromic Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        string s1 = s;
+        reverse(s.begin(),s.end());
+        string s2 = s;
+        int n1 = s1.size(),n2 = s2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(s2[i-1]==s1[j-1])  dp[i][j] = 1 + dp[i-1][j-1];
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n1][n2];
+    }
+};
