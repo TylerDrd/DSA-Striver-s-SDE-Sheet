2e5ee4439c7e9901555ae2cdadfd63838fbf1043
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2024-03-04 22:01:33 +0530

    Create Longest Common Substring.cpp

---
+++ Dynamic Programming/DP on strings/Longest Common Substring.cpp
@@ -0,0 +1,20 @@
+int lcs(string &text1, string &text2){
+    // Write your code here.
+         int n1 = text1.size(),n2 = text2.size();
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
+        int ans = 0;
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                    ans = max(ans,dp[i][j]);
+                }  
+                else dp[i][j] = 0;
+            }
+        }
+        return ans;
+
+}
