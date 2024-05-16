4c319dc3fb53605f348acb75fee509845ed1e764
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2024-05-16 23:11:05 +0530

    Create Roman to Integer.cpp

---
+++ Strings/Medium/Roman to Integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    unordered_map<char,int> m = {
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000}
+    };
+    int romanToInt(string s) {
+        int ans = 0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(i==s.size()-1) ans+=m[s[i]];
+            else if(m[s[i]] >= m[s[i+1]]) ans+=m[s[i]];
+            else ans-=m[s[i]];
+        }
+        return ans;
+    }
+};
