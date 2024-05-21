9fde4706d6e00372d151e5c7b84f01d6f34f8df6
Jainil Patel <116826632+TylerDrd@users.noreply.github.com>
2024-05-21 10:58:04 +0530

    Update Find eventual safe states.cpp

--- Graphs/Topo Sort and Problems/Find eventual safe states.cpp
+++ Graphs/Topo Sort and Problems/Find eventual safe states.cpp
@@ -1,31 +1,35 @@
-#include <bits/stdc++.h> 
-vector<int> safeNodes(vector<vector<int>> &edges, int n, int e) {
-    // Write your code here.
-    vector<vector<int>> adj(n);
-    vector<int> indeg(n,0);
-    for(int i=0;i<e;i++)
-    {
-        adj[edges[i][1]].push_back(edges[i][0]);
-        indeg[edges[i][0]]++;
-    }
-
-    queue<int> q;
-    vector<int> ans;
-    for(int i=0;i<n;i++)
-    {
-        if(indeg[i]==0) q.push(i);
-    }
-    while(!q.empty())
-    {
-        int t = q.front();
-        ans.push_back(t);
-        q.pop();
-        for(auto it : adj[t])
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++)
         {
-            indeg[it]--;
-            if(indeg[it]==0) q.push(it);
+            for(auto it : graph[i]) adj[it].push_back(i);
         }
+        vector<int> ind(n,0);
+        for(int i=0;i<n;i++)
+        {
+            for(auto it : adj[i]) ind[it]++;
+        }
+        queue<int> q;
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(ind[i]==0) q.push(i);
+        }
+        while(!q.empty())
+        {
+            auto t = q.front();
+            q.pop();
+            ans.push_back(t);
+            for(auto it : adj[t])
+            {
+                ind[it]--;
+                if(ind[it]==0) q.push(it);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
     }
-    sort(ans.begin(),ans.end());
-    return ans;
-}
+};
